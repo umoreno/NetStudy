7b726b3b05ddbc3445dcd71d8587b511f9d1c6c9
Ulises Moreno Ramon <umoreno@emersonecologics.com>
2016-12-25 13:51:57 -0500

    Adding files
    netcore files for threading example

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      // Create the token source.
+      CancellationTokenSource cts = new CancellationTokenSource();
+
+      // Pass the token to the cancelable operation.
+      ThreadPool.QueueUserWorkItem(new WaitCallback(DoSomeWork), cts.Token);
+      Thread.Sleep(2500);
+
+      // Request cancellation.
+      cts.Cancel();
+      Console.WriteLine("Cancellation set in token source...");
+      Thread.Sleep(2500);
+      // Cancellation should have happened, so call Dispose.
+      cts.Dispose();
+   }
+   // Thread 2: The listener
+   static void DoSomeWork(object obj)
+   {
+      CancellationToken token = (CancellationToken)obj;
+
+      for (int i = 0; i < 10000000; i++) {
+         if (token.IsCancellationRequested)
+         {
+            Console.WriteLine("In iteration {0}, cancellation has been requested...",
+                              i + 1);
+            // Perform cleanup if necessary.
+            //...
+            // Terminate the operation.
+            break;
+         }
+         else
+         {
+             Console.WriteLine("No cancelation");
+         }
+         // Simulate some work.
+         //Thread.Wait(500000);
+      }
+   }
+}
+// The example displays output like the following:
+//       Cancellation set in token source...
+//       In iteration 1430, cancellation has been requested...
